3aff4681e878508a8d3c7468c61ca4dfe0fbd470
Alexis <alexis.mathiot@gmail.com>
2023-08-18 14:22:16 +0200

    change bootstrap version 5.02 -> 3.4.1

---
+++ src/Security/Voter/TaskVoter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Task;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class TaskVoter extends Voter
+{
+    public const DELETE = 'CAN_DELETE';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        return $attribute === self::DELETE
+            && $subject instanceof Task;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        return $subject->getUser() === $user || ($subject->getUser() === null && in_array('ROLE_ADMIN', $user->getRoles(), true));
+    }
+}
